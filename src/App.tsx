18bc7e9b6f924b93faf099c7f3823b31a095c805
devlavinea <lavinea2411@gmail.com>
2026-07-16 19:11:02 -0300

    Ajustes layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import noivos from "./assets/noivos.png";
 import fundoEsquerdo from "./assets/fundo_esquerdo.png";
 import fundoDireito from "./assets/fundo_direito.png";
 import lacre from "./assets/lacre.png";
+import ornamento2 from "./assets/ornamento2.png";
+import ornamento3 from "./assets/ornamento3.png";
+import ornamento1 from "./assets/ornamento1.png";
+
+
 
 function App() {
   const [abrindo, setAbrindo] = useState(false);
@@ -18,25 +23,35 @@ function App() {
   };
 
   return (
-    <section id='convite' className='  w-screen h-full bg-[#f5f5f5] relative overflow-hidden'>
+    <section id='convite' className=' w-screen h-screen bg-[#f5f5f5] flex justify-center overflow-hidden'>
 
      
-      <div className=" overflow-y-scroll  convite h-screen  w-full justify-center items-center flex flex-col relative">
-      <span className="h1 text-[#4a5c36] text-[17px] absolute bottom-62 "> 02 de agosto de 2026</span>
+      <div className=" relative pt-15  convite h-screen  w-full justify-center text-center items-center flex flex-col gap-0.5 ">
+    
+    <p className="text-[17px]  leading-[1.2] text-[#4a5c36]">
+      Cada família tem uma hitória.<br></br> Bem-vindo(a) ao começo da nossa.
+    </p>
+    <img src={ornamento2} className="h-5.5 "></img>
+    <img src={noivos} className="w-60  "></img>
+         <img src={ornamento2} className=" h-5.5"></img>
+
+      <span className=" text-[#4a5c36] text-[17px] "> 02 de agosto de 2026</span>
+        <img src={ornamento3} className=" h-6"></img>
+
     <button  onClick={() =>
     window.open(
       "https://maps.google.com/?q=-9.389083,-40.503096",
       "_blank"
     )
-  } className="shadow-button absolute bottom-43 w-43 h-10 bg-[#4a5c36] text-white rounded-md hover:bg-[#3a4c26] flex items-center justify-center gap-4">
+  } className="shadow-button mt-2   w-43 h-10 bg-[#4a5c36] text-white rounded-md hover:bg-[#3a4c26] flex items-center justify-center gap-4">
   <svg className="size-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
   </svg>
   Local do evento
 </button>
-<Link className="absolute bottom-30" to="/lista">
-   <button className="shadow-button  w-43 h-10 bg-[#4a5c36] text-white rounded-md hover:bg-[#3a4c26] flex items-center justify-center gap-2">
+<Link className=" " to="/lista">
+   <button className="shadow-button mt-2  w-43 h-10 bg-[#4a5c36] text-white rounded-md hover:bg-[#3a4c26] flex items-center justify-center gap-2">
   <svg className="size-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
     <path
       strokeLinecap="round"
@@ -47,11 +62,13 @@ function App() {
   Lista de presentes
 </button> 
 </Link> 
+<img src={ornamento1} className=" h-8 mt-3"></img>
+<p className=" text-[#4a5c36] leading-tight text-[14px]">Com carinho,<br></br> esperamos você!</p>
         </div>
 
 
 
- <section id="capa-convite" className=" absolute z-10 flex justify-center w-screen h-screen perspective">
+ <section id="capa-convite" className="hidden absolute z-10 flex justify-center w-screen h-screen perspective">
       {/* Página esquerda */}
       <div
         className={`relative w-1/2 h-screen shadow z-10 ${
