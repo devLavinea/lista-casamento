be313c87faa9d3ffd805e291480e725a13ae3605
Lavinea Souza <lavinea2411@gmail.com>
2026-08-20 16:10:04 -0300

    Update list.tsx

--- src/list.tsx
+++ src/list.tsx
@@ -24,11 +24,11 @@ function List() {
   const textoAviso = 
     `O presente é totalmente opcional!
     
-    Caso você escolha nos presentear,será recebido com muito carinho e alegria. 
-    
-    Desde já, agradecemos imensamente por fazer parte desse momento tão especial para nós. 💚
+    Caso você escolha nos presentear, será recebido com muito carinho e alegria. 
 
-    Se preferir, você também pode se juntar a um amigo ou familiar e dividir o valor do presente.`;
+    Se preferir, você também pode se juntar a um amigo ou familiar e dividir o valor do presente.
+    
+    Desde já, agradecemos imensamente pelo carinho. 💚`;
 
   // BUSCAR PRESENTES
   async function buscarPresentes() {
