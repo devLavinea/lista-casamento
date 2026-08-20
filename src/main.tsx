43990e754c1d9197c58ee776f1a2ae9877131b2a
Lavinea Souza <lavinea2411@gmail.com>
2026-08-20 19:55:10 -0300

    Add route for Local component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 
 import App from "./App";
 import List from "./list";
+import Local from "./local";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/lista" element={<List />} />
+        <Route path="/local" element={<Local />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
